03ee217ec96810e61b7ac9b5cd661ccd0f5d2707
Motoyinc <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjuEzoMHvLSpwbIolRwO6KfZkpva5wWEFwZhZL9P0wz Motoyinc@gmail.com>
2024-03-24 07:10:45 +0800

    修改一下更新更新脚本

--- python/app/app.py
+++ python/app/app.py
@@ -7,7 +7,8 @@ import uuid
 app = Flask(__name__)
 executor = Executor(app)  # 初始化Executor
 tasks = {}
-
+new_url = "https://github.com/Motoyinc/gitRule.git"
+remote_name = 'origin'
 @app.route('/start-pullRule', methods=['POST'])
 def execute_python_script():
     data = request.json
@@ -26,18 +27,44 @@ def task_status(task_id):
             return jsonify(tasks[task_id]), 200
     else:
         return jsonify({"error": "Invalid task ID"}), 404
+def get_git_repo_path():
+    try:
+        # 使用git命令获取顶层仓库路径
+        repo_path = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode().strip()
+        return repo_path
+    except subprocess.CalledProcessError:
+        # 如果当前目录不在git仓库中，返回错误
+        return "当前目录不在Git仓库中。"
 
+
+# 当前路径 /app/app.py
 @app.route('/update-pullRule', methods=['POST'])
 def update_pull_rule():
-    repo_url = "https://github.com/Motoyinc/gitRule.git"
-    clone_path = "/Script/gitRule"
+    # 获取当前脚本的绝对路径
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    git_rule_path = os.path.join(script_dir, '../gitRule')  # 生成gitRule目录的绝对路径
+    os.makedirs(git_rule_path, exist_ok=True)  # 确保目录存在，如果不存在则创建
+    try:
+        # 切换到gitRule目录
+        os.chdir(git_rule_path)
+        # 更新远端URL
+        subprocess.check_output(['git', 'remote', 'set-url', remote_name, new_url], stderr=subprocess.STDOUT)
+        print(f"远端 '{remote_name}' 的 URL 已更新为: {new_url}")
+        try:
+            # 拉取远程仓库的最新内容
+            subprocess.check_output(['git', 'pull', remote_name, 'master'], stderr=subprocess.STDOUT)  # 指定远端名称和分支
+            print("仓库已成功更新。")
+        except subprocess.CalledProcessError as e:
+            print(f"更新仓库时出错：{e.output.decode()}")
+            return jsonify({"error": "Error pulling repository"}), 500
+    except subprocess.CalledProcessError as e:
+        print(f"操作时出错: {e.output.decode()}")
+        return jsonify({"error": "Operation failed"}), 500
+    finally:
+        # 无论操作成功与否，都切换回脚本所在的原始目录
+        os.chdir(script_dir)
 
-    if not os.path.exists(clone_path):
-        subprocess.run(['git', 'clone', repo_url, clone_path], check=True)
-    else:
-        subprocess.run(['git', '-C', clone_path, 'pull'], check=True)
-
-    return jsonify({"message": "Repository updated successfully"}), 200
+    return jsonify({"message": "gitRuleApp update succeed"}), 200
 
 def long_running_task(data, task_id):
     script_name = data.get("script_name", "default_script.py")
